3fed0f151b18d08b07fb368f12b73e099055fb58
nel.sokchhunly19 <sokchhunlynel@gmail.com>
2025-05-18 00:21:01 +0700

    chore: setup react-query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div>
+    <div className="min-h-screen w-full flex justify-center items-center">
       <h1>PS Collection Store</h1>
     </div>
   );
